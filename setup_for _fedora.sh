b9ca4503c5a34ad6ca7e75096472fb6b8f5db8af
thomas devasia <tdevasia1997@gmail.com>
2023-07-08 16:32:22 +0100

    base folders

---
+++ setup_for _fedora.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Update configuration files
+update_configs() {
+    # Update config_file1.conf
+    # Example: sed -i 's/old_value/new_value/g' /path/to/config_file1.conf
+
+    # Update config_file2.conf
+    # Example: sed -i 's/old_value/new_value/g' /path/to/config_file2.conf
+
+    # Update config_file3.conf
+    # Example: sed -i 's/old_value/new_value/g' /path/to/config_file3.conf
+}
+
+# Check for package updates
+check_updates() {
+    sudo dnf update -y
+}
+
+# Install packages from a script
+install_packages() {
+    # Install package1
+    sudo dnf install polybar
+
+}
+
+# Run the script
+update_configs
+check_updates
+install_packages
