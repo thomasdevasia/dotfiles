6562d7916e5e9817fcfc637c3fb2ef42d76e8c0b
thomas devasia <tdevasia1997@gmail.com>
2023-07-09 19:45:37 +0100

    wallpaper selector fixed

--- applications/wallpaper-selector/set_wallpaper.sh
+++ applications/wallpaper-selector/set_wallpaper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Run Pywal to generate and apply a color scheme
-wal -i ~/Pictures/
+echo "before wal"
+selection=$(ls ~/Pictures | /home/thomasdevasia/.config/rofi/dmenu/launcher.sh)
+~/.local/bin/wal -i ~/Pictures/$selection
